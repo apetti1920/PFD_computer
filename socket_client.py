dde6edb777655d285ccc7a7fbb703e8dff6ea2ef
Aidan <apetti1920@aol.com>
2018-10-15 22:13:12 -0400

    Origonal Commit 2

--- socket_client.py
+++ socket_client.py
@@ -6,26 +6,21 @@ class Client():
     def __init__(self):
         self.start_client()
 
+
     def start_client(self):
-        host = socket.gethostname()  # as both code is running on same pc
-        port = 5000  # socket server port number
+        import socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 50000))
+        t1 = Thread(target=self.client_listen, args=(s,))
+        t1.start()
 
-        client_socket = socket.socket()  # instantiate
-        client_socket.connect((host, port))  # connect to the server
+        while True:
+            s.sendall('hi')
 
-        print('Connected to', client_socket.getsockname())
-
-        message = input(" -> ")  # take input
-
-        while message.lower().strip() != 'bye':
-            client_socket.send(str(message))  # send message
-            data = client_socket.recv(1024)  # receive response
-
-            print('Received from server: ' + data)  # show in terminal
-
-            message = input(" -> ")  # again take input
-
-        client_socket.close()  # close the connection
+    def client_listen(self, sock):
+        while True:
+            data = sock.recv(1024)
+            print(data)
 
 
 if __name__ == '__main__':
--- socket_server.py
+++ socket_server.py
@@ -1,5 +1,6 @@
 import socket
 from threading import Thread
+import time
 
 
 class Server():
@@ -7,31 +8,25 @@ class Server():
         self.start_server()
 
     def start_server(self):
-        # get the hostname
-        host = socket.gethostname()
-        port = 8888  # initiate port no above 1024
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('localhost', 50000))
+        s.listen(1)
+        conn, addr = s.accept()
+        t1 = Thread(target=self.server_send, args=(conn,), daemon=True)
+        t1.start()
 
-        server_socket = socket.socket()  # get instance
-        # look closely. The bind() function takes tuple as argument
-        server_socket.bind((host, port))  # bind host address and port together
-
-        # configure how many client the server can listen simultaneously
-        server_socket.listen(1)
-        conn, address = server_socket.accept()  # accept new connection
-        print('Connection from Address', address)
-
-        while True:
-            # receive data stream. it won't accept data packet greater than 1024 bytes
+        while 1:
             data = conn.recv(1024)
-            print('data', data)
             if not data:
-                # if data is not received break
                 break
-            print("from connected user: " + str(data))
-            data = input(' -> ')
-            conn.send(str(data))  # send data to the client
 
-        conn.close()  # close the connection
+            print(data)
+
+        conn.close()
+
+    def server_send(self, conn):
+        time.sleep(5)
+        conn.sendall('stop')
 
 
 if __name__ == '__main__':
