2736bc1cc4cfd8c7dd46213644ca65d06d687ffe
Aidan <apetti1920@aol.com>
2018-10-16 15:06:27 -0400

    Origonal Commit 4

--- socket_client.py
+++ socket_client.py
@@ -3,12 +3,19 @@ from threading import Thread
 from random import randrange
 import json
 import time
+import sys
+
+from noise import pnoise1
+
 
 class Client():
     def __init__(self):
         self.sock = self.start_client()
+        self.frame = 0
 
-        t2 = Thread(target=self.client_listen)
+
+
+        t2 = Thread(target=self.client_listen, daemon=True)
         t2.start()
 
         while True:
@@ -16,16 +23,19 @@ class Client():
             j = json.dumps(temp)
             try:
                 self.client_send(j)
-                time.sleep(1)
+                time.sleep(3)
             except Exception as e:
-                print('error', e)
+                pass
 
-
-    def start_client(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('localhost', 50000))
-
-        return s
+    @staticmethod
+    def start_client():
+        while True:
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect(('localhost', 50000))
+                return s
+            except Exception as e:
+                pass
 
     def client_send(self, message):
         t1 = Thread(target=self.sock.sendall, args=(message,))
@@ -37,7 +47,10 @@ class Client():
             print(data)
 
     def read_sensors(self):
-        return {'data': {'depth': randrange(0,100,2), 'IMUx': randrange(0,100,3), 'IMUy': randrange(0,100,2), 'IMUz': randrange(0,100,3)}}
+        self.frame += 1
+        return {'data': {'depth': pnoise1(self.frame * .0013), 'IMUx': pnoise1(self.frame * -.0023),
+                         'IMUy': pnoise1(self.frame * .0073),
+                         'IMUz': pnoise1(self.frame * -.0003)}}
 
 
 if __name__ == '__main__':
