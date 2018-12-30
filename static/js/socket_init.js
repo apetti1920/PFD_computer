d9bb6fac73ca7864af9a96616f0284bac835871a
apetti1920 <apetti1920@aol.com>
2018-12-30 00:52:25 -0500

    Add files via upload

--- static/js/socket_init.js
+++ static/js/socket_init.js
@@ -9,12 +9,18 @@ socket.on('message', function(data) {
 
 function buttonclick() {
     const elem = document.getElementById("arm_button");
-    if (elem.innerHTML == 'Arm Device'){
-        socket.emit('button click', {data: 'Arm'});
-        elem.innerHTML = 'Disarm Device'
+    if (elem.innerHTML === 'Arm Device'){
+        b1 = document.getElementById("bladder1").value;
+        b2 = document.getElementById("bladder2").value;
+        b3 = document.getElementById("bladder3").value;
+        b4 = document.getElementById("bladder4").value;
+
+        data = {data: 'Arm', times: {B1: b1, B2: b2, B3: b3, B4: b4}};
+        socket.emit('button click', data);
+        elem.innerHTML = 'Disarm Device';
     }
-    else if (elem.innerHTML == 'Disarm Device'){
+    else if (elem.innerHTML === 'Disarm Device'){
         socket.emit('button click', {data: 'Disarm'});
-        elem.innerHTML = 'Arm Device'
+        elem.innerHTML = 'Arm Device';
     }
 }
